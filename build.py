459629328484e0dfce69b67fdf121fd0cdb3ea3a
Mingyi <mingyi.z@outlook.com>
2018-11-25 15:48:38 +0800

    update ci to conanos pattern

--- build.py
+++ build.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from conanos.build import Main
 
-import os
-from bincrafters import build_template_default
-
-os.environ['CONAN_USERNAME'] = os.environ.get('CONAN_USERNAME','conanos')
-
-if __name__ == "__main__":
-
-    builder = build_template_default.get_builder(pure_c=True)
-
-    items = []
-    for item in builder.items:
-        if not (item.settings["compiler"] == "Visual Studio" and item.settings["compiler.version"] == "12"):
-            items.append(item)
-    builder.items = items
-
-    builder.run()
+if __name__ == "__main__":    
+    Main('opus',pure_c=True)
